c45503e1d442b87f669397eaabef045c3e47f249
lights0n <lyadouskiy@gmail.com>
2022-12-20 16:59:04 +0300

    feat:footer implemention

--- src/global/components/footer/index.ts
+++ src/global/components/footer/index.ts
@@ -1,11 +1,34 @@
 import Component from '../../templates/component';
 
+const footerButtons = [
+    {
+        tag: 'a',
+        innerText: 'Lights0n',
+        // backgroundImage: '#',
+        href: '#https://github.com/Lights0n',
+    },
+];
+
 class Footer extends Component {
     constructor(tagName: string, className: string) {
         super(tagName, className);
     }
 
+    renderFooterButtons(): void {
+        const footerBody = document.createElement('div');
+        footerBody.classList.add('footer__body');
+
+        footerButtons.forEach((element, index) => {
+            const itemHTML: string | HTMLElement = document.createElement(`${element.tag}`);
+            console.log(Object.values(element));
+            for (let i = 1; i < Object.values(element).length; i++) {
+                itemHTML.[Object.keys(element)[i]] = Object.values(element)[i];
+            }
+        });
+    }
+
     render() {
+        this.renderFooterButtons();
         return this.container;
     }
 }
