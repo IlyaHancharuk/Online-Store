5b13227178c214a35e6db17917d74a0d67094310
Lights0n <lyadouskiy@gmail.com>
2023-01-04 22:44:19 +0300

    feat: implement counter

--- src/pages/cart/index.ts
+++ src/pages/cart/index.ts
@@ -42,6 +42,9 @@ class CartPage extends Page {
         }
         return;
     }
+    private removeFromList(listItem: HTMLElement): void {
+        // const elem = this.container.querySelector()
+    }
 
     private addItemViaTemplate(itemId: number, itemAmount: number): void {
         // find item via itemId
@@ -59,9 +62,22 @@ class CartPage extends Page {
         const itemRating = cartClone.querySelector('.item__rating');
         const itemDiscount = cartClone.querySelector('.item__discount');
         const itemStock = cartClone.querySelector('.item__in-stock');
+        const itemPlus = cartClone.querySelector('.item__amount-plus');
+        const itemMinus = cartClone.querySelector('.item__amount-minus');
         const itemHowMany: HTMLInputElement | null = cartClone.querySelector('.item__current-amount');
 
-        if (itemNum && itemPhoto && itemTitle && itemDescr && itemRating && itemDiscount && itemStock && itemHowMany) {
+        if (
+            itemNum &&
+            itemPhoto &&
+            itemTitle &&
+            itemDescr &&
+            itemRating &&
+            itemDiscount &&
+            itemStock &&
+            itemMinus &&
+            itemHowMany &&
+            itemPlus
+        ) {
             itemNum.textContent = '2';
             itemPhoto.setAttribute('src', `${product.thumbnail}`);
             itemTitle.textContent = product.title;
@@ -70,6 +86,29 @@ class CartPage extends Page {
             itemDiscount.textContent = `Discount: ${product.discountPercentage}%`;
             itemStock.textContent = `Stock: ${product.stock}`;
             itemHowMany.value = `${itemAmount}`;
+            // пока не робит. Но робит иначе
+            itemHowMany.max = `${product.stock}`;
+            // делаем кнопки + и - рабочими
+            itemMinus.addEventListener('click', (): void => {
+                if (+itemHowMany.value - 1 <= 0) {
+                    itemNum.parentElement?.remove();
+                }
+                itemHowMany.value = `${+itemHowMany.value - 1}`;
+                this.decreaseFromCart(`${product.id}`, '1');
+                // чекаем, последний ли элемент был.
+                this.sayCartIsEmpty();
+            });
+            itemPlus.addEventListener('click', () => {
+                if (+itemHowMany.value >= product.stock) {
+                    itemHowMany.value = `${+itemHowMany.value}`;
+                    this.decreaseFromCart(`${product.id}`, '0');
+                    return;
+                }
+                // просто вызываем с минусом. Функция уменьшения почитает красиво
+                itemHowMany.value = `${+itemHowMany.value + 1}`;
+                this.decreaseFromCart(`${product.id}`, '-1');
+                return;
+            });
         }
 
         fragment.append(cartClone);
@@ -82,19 +121,22 @@ class CartPage extends Page {
         }
     }
 
+    // ! можно переписать под чуть красивее. Но кукуха пока не варит. Работает превосходно
     private sayCartIsEmpty(): void {
-        const emptyCartPageText = this.createElement('p', 'cart__empty-text');
-        emptyCartPageText.innerText = 'The cart is empty yet ¯\\_( ◡ ₒ ◡ )_/¯';
-        this.container.append(emptyCartPageText);
-        return;
-    }
-
-    private removeSayCartIsEmpty(): void {
-        const message = this.container.querySelector('.cart__empty-text');
-        if (message) {
-            message.remove();
+        if (localStorage['RS-store-data']) {
+            const localData = JSON.parse(localStorage['RS-store-data']);
+            if (localData.length === 0) {
+                localStorage.clear();
+                const emptyCartPageText = this.createElement('p', 'cart__empty-text');
+                emptyCartPageText.innerText = 'The cart is empty yet ¯\\_( ◡ ₒ ◡ )_/¯';
+                this.container.append(emptyCartPageText);
+            }
+        } else {
+            const emptyCartPageText = this.createElement('p', 'cart__empty-text');
+            emptyCartPageText.innerText = 'The cart is empty yet ¯\\_( ◡ ₒ ◡ )_/¯';
+            this.container.append(emptyCartPageText);
+            return;
         }
-        return;
     }
 
     // ? тащим метод класса из другого класса. хохох
@@ -112,8 +154,17 @@ class CartPage extends Page {
         return;
     }
 
-    decreaseFromCart(itemId: string, howMuchToReduce = '1'): void {
+    decreaseFromCart(itemId: string, howMuchToReduce = '1'): string {
         this.cartInf.reduceItemAmount(itemId, howMuchToReduce);
+        return '1';
+    }
+
+    private removeSayCartIsEmpty(): void {
+        const message = this.container.querySelector('.cart__empty-text');
+        if (message) {
+            message.remove();
+        }
+        return;
     }
 
     private addItemsfromLocalStorage(): void {
@@ -138,12 +189,14 @@ class CartPage extends Page {
     render() {
         this.createCartBodyHTML();
         // !Чисти localStorage чтобы при обновах старого не висело
+
         // добавить по стандарту 24 предмета
         // удалить 1 (без 2 аргумента по стандарту)
         // удалить 20 из них
-        this.addToCart('20', '24');
-        this.decreaseFromCart('20');
-        this.decreaseFromCart('20', '20');
+        /*  this.addToCart('3', '24');
+         this.decreaseFromCart('3');
+         this.decreaseFromCart('3', '20'); */
+
         this.addItemsfromLocalStorage();
         return this.container;
     }
