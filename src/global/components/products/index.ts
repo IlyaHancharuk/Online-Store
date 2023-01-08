5e3a07cf63da45cbb7530fcddda638929268ccee
Ilya Hancharuk <innot707@gmail.com>
2023-01-09 00:33:24 +0300

    feat: add tag 'img' for product images with 'loading=lazy' attribute

--- src/global/components/products/index.ts
+++ src/global/components/products/index.ts
@@ -115,11 +115,15 @@ class Products extends Component {
                     const detailsButton = productClone.querySelector<HTMLElement>('.details-button');
 
                     if (productItem && productText && itemTitle && itemInfo && dropButton && detailsButton) {
+                        const itemImage = document.createElement('div');
+                        itemImage.className = 'item__image';
                         const img = new Image();
+                        img.loading = 'lazy';
+                        img.style.position = 'absolute';
                         img.src = item.thumbnail;
-                        img.onload = () => {
-                            productItem.style.backgroundImage = `url(${item.thumbnail})`;
-                        };
+                        img.alt = `${item.title} image`;
+                        itemImage.append(img);
+                        productItem.append(itemImage);
 
                         productText.addEventListener('click', () => {
                             window.location.hash = `product-details-page/${item.id.toString()}`;
